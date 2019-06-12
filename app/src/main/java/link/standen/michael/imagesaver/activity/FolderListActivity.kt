28fcce2ea9a730adeac1614b39a84b7b3bc469b9
Michael Standen <screaminghawk@gmail.com>
2019-06-12 20:32:37 +1200

    Add storage permissions

--- app/src/main/java/link/standen/michael/imagesaver/activity/FolderListActivity.kt
+++ app/src/main/java/link/standen/michael/imagesaver/activity/FolderListActivity.kt
@@ -1,6 +1,8 @@
 package link.standen.michael.imagesaver.activity
 
 import android.app.ListActivity
+import android.content.pm.PackageManager
+import android.util.Log
 import link.standen.michael.imagesaver.R
 
 /**
@@ -8,6 +10,11 @@ import link.standen.michael.imagesaver.R
  * This activity shows the list of user selected folders.
  */
 class FolderListActivity : ListActivity() {
+
+	companion object {
+		const val TAG = "FolderListActivity"
+		const val PERMISSION_REQUEST_CODE = 2
+	}
 
 	override fun onCreate(savedInstanceState: android.os.Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -18,6 +25,26 @@ class FolderListActivity : ListActivity() {
 			//TODO FAB implementation
 		}
 
+		// Check for permissions
+		testPermissions()
+
 		//TODO List adapter
+	}
+
+	/**
+	 * Test permissions are granted.
+	 */
+	private fun testPermissions(){
+		val permissions = listOf(
+			// All available permissions
+			android.Manifest.permission.READ_EXTERNAL_STORAGE,
+			android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+			// Filter out granted permissions
+		).filter { checkSelfPermission(it) != PackageManager.PERMISSION_GRANTED }.toTypedArray()
+		if (permissions.isNotEmpty()){
+			Log.i(TAG, "Requesting permission for " + permissions.reduce { total, next -> "$total, $next" })
+			// Request permissions
+			requestPermissions(permissions, PERMISSION_REQUEST_CODE)
+		}
 	}
 }
